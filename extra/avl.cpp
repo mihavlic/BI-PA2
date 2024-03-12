0d6ce82348e4e1f29b0d9183f01c80ade97799ca
Michal Havlíček <havlim39@fit.cvut.cz>
2024-03-12 17:11:28 +0100

    Fix avl tree

--- extra/avl.cpp
+++ extra/avl.cpp
@@ -52,7 +52,7 @@ int node_get_balance(NodeIndex index, std::vector<Node> &arena) {
 
 int max(int a, int b) { return (a > b) ? a : b; }
 
-void node_update_for_children(Node *node, std::vector<Node> &arena) {
+void node_update_height(Node *node, std::vector<Node> &arena) {
   Node *left = get_node(node->left, arena);
   Node *right = get_node(node->right, arena);
 
@@ -82,8 +82,8 @@ NodeIndex node_rotate_left(NodeIndex y_index, std::vector<Node> &arena) {
   x->left = y_index;
   y->right = z_index;
 
-  node_update_for_children(y, arena);
-  node_update_for_children(x, arena);
+  node_update_height(y, arena);
+  node_update_height(x, arena);
 
   return x_index;
 }
@@ -109,8 +109,8 @@ NodeIndex node_rotate_right(NodeIndex y_index, std::vector<Node> &arena) {
   x->right = y_index;
   y->left = z_index;
 
-  node_update_for_children(y, arena);
-  node_update_for_children(x, arena);
+  node_update_height(y, arena);
+  node_update_height(x, arena);
 
   return x_index;
 }
@@ -125,45 +125,40 @@ NodeIndex push_node(int key, std::vector<Node> &arena) {
   return inserted_index;
 }
 
-NodeIndex maybe_rebalance_node(Node *root, NodeIndex index, int key,
+NodeIndex maybe_rebalance_node(Node *root, NodeIndex index,
                                std::vector<Node> &arena) {
-  node_update_for_children(root, arena);
+  node_update_height(root, arena);
 
   int balance = node_get_balance(index, arena);
 
-  // if this node becomes unbalanced, then there are 4 cases
-  Node *left_node = get_node(root->left, arena);
-  Node *right_node = get_node(root->right, arena);
+  // left heavy
+  if (balance > 1) {
+    if (node_get_balance(root->left, arena) == -1) {
+      NodeIndex n = node_rotate_left(root->left, arena);
 
-  // Left Left Case
-  if (balance > 1 && key < left_node->key) {
+      // root possibly invalidated
+      root = get_node(index, arena);
+      root->left = n;
+    }
+
     return node_rotate_right(index, arena);
   }
 
-  // Left Right Case
-  if (balance > 1 && key > left_node->key) {
-    NodeIndex n = node_rotate_left(root->left, arena);
-    // root possibly invalidated
-    root = get_node(index, arena);
-    root->left = n;
-    return node_rotate_right(index, arena);
-  }
+  // right heavy
+  if (balance < -1) {
+    if (node_get_balance(root->right, arena) == 1) {
+      NodeIndex n = node_rotate_right(root->right, arena);
 
-  // Right Left Case
-  if (balance < -1 && key < right_node->key) {
-    NodeIndex n = node_rotate_right(root->right, arena);
-    // root possibly invalidated
-    root = get_node(index, arena);
-    root->right = n;
+      // root possibly invalidated
+      root = get_node(index, arena);
+      root->right = n;
+    }
+
     return node_rotate_left(index, arena);
   }
 
-  // Right Right Case
-  if (balance < -1 && key > right_node->key) {
-    return node_rotate_left(index, arena);
-  }
-
-  node_update_for_children(root, arena);
+  // height is otherwise updated when rotating
+  node_update_height(root, arena);
   return index;
 }
 
@@ -193,16 +188,17 @@ NodeIndex insert_node(NodeIndex index, int key, std::vector<Node> &arena) {
   }
 
   // we've inserted a node into one of our children, rebalance
-  return maybe_rebalance_node(root, index, key, arena);
+  return maybe_rebalance_node(root, index, arena);
 }
 
-NodeIndex node_get_leftmost(NodeIndex node, std::vector<Node> &arena) {
-  NodeIndex current = node;
-  Node *next = NULL;
-  while ((next = get_node(current, arena))) {
-    current = next->left;
+NodeIndex node_get_leftmost(NodeIndex index, std::vector<Node> &arena) {
+  NodeIndex prev = -1;
+  NodeIndex next = index;
+  while (Node *node = get_node(next, arena)) {
+    prev = next;
+    next = node->left;
   }
-  return current;
+  return prev;
 }
 
 NodeIndex delete_node(NodeIndex index, int key, std::vector<Node> &arena) {
@@ -223,17 +219,17 @@ NodeIndex delete_node(NodeIndex index, int key, std::vector<Node> &arena) {
     root = get_node(index, arena);
     root->right = n;
   } else {
-    Node *left_node = get_node(root->left, arena);
-    Node *right_node = get_node(root->left, arena);
+    NodeIndex has_left = root->left >= 0;
+    NodeIndex has_right = root->right >= 0;
 
-    if (!left_node && !right_node) { // no children
-      return (NodeIndex)-1;
-    } else if (!right_node) { // only left
+    if (!has_left && !has_right) { // no children
+      return -1;
+    } else if (!has_right) { // only left
       return root->left;
-    } else if (!left_node) { // only right
+    } else if (!has_left) { // only right
       return root->right;
     } else { // both children
-      // Get the inorder successor (smallest in the right subtree)
+      // get the inorder successor (smallest in the right subtree)
       NodeIndex leftmost_index = node_get_leftmost(root->right, arena);
       Node *leftmost = get_node(leftmost_index, arena);
       // reparent the leftmost into root
@@ -247,7 +243,7 @@ NodeIndex delete_node(NodeIndex index, int key, std::vector<Node> &arena) {
   }
 
   // we've removed one of our children, rebalance
-  return maybe_rebalance_node(root, index, key, arena);
+  return maybe_rebalance_node(root, index, arena);
 }
 
 void node_print(NodeIndex index, std::vector<Node> &arena, int indent) {
@@ -262,18 +258,36 @@ void node_print(NodeIndex index, std::vector<Node> &arena, int indent) {
   }
 }
 
+void node_print_order(NodeIndex index, std::vector<Node> &arena, int indent) {
+  Node *node = get_node(index, arena);
+  if (node) {
+    node_print_order(node->left, arena, indent + 1);
+    printf("%d ", node->key);
+    node_print_order(node->right, arena, indent + 1);
+  }
+  if (indent == 0) {
+    printf("\n");
+  }
+}
+
 int main() {
   std::vector<Node> arena{};
   NodeIndex root = -1;
 
   for (int i = 0; i < 64; i++) {
     int value = (i * i + ~i) % 64;
-    printf("%d ", value);
     root = insert_node(root, value, arena);
   }
-  printf("\n");
 
-  node_print(root, arena, 0);
+  node_print_order(root, arena, 0);
+
+  for (int i = 0; i < 64; i++) {
+    int value = (i * i + ~i) % 64;
+    root = delete_node(root, value, arena);
+  }
+
+  printf("\n");
+  node_print_order(root, arena, 0);
 
   return 0;
 }
