cb8faa6350cedab8984572d4b53097a1c1280d59
Michal Havlíček <havlim39@fit.cvut.cz>
2024-03-16 12:44:51 +0100

    Add some untested avl methods

--- extra/avl.cpp
+++ extra/avl.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 
 template <typename T> using KeyOrdering = int(T *, T *);
+template <typename T> using KeySearch = int(T *);
+template <typename T> using KeySearchPredicate = bool(T *);
 
 // AVL tree modified to use arena allocation
 // https://www.geeksforgeeks.org/insertion-in-an-avl-tree/
@@ -85,6 +87,10 @@ template <typename T> struct Node {
   static void maybe_rebalance_node(Node **node) {
     Node *root = *node;
 
+    if (root == nullptr) {
+      return;
+    }
+
     root->update_height();
     int balance = root->get_balance();
 
@@ -102,9 +108,63 @@ template <typename T> struct Node {
       node_rotate_left(node);
     }
   }
+  static void find_min_by(Node<T> **node, KeySearchPredicate<T> search,
+                          std::vector<Node<T> **> &path) {
+    while (true) {
+      Node<T> *root = *node;
+
+      if (root == NULL || !search(&root->left->key)) {
+        return;
+      }
+
+      path.push_back(node);
+      node = &root->left;
+    }
+  }
+  static void find_max_by(Node<T> **node, KeySearchPredicate<T> search,
+                          std::vector<Node<T> **> &path) {
+    while (true) {
+      Node<T> *root = *node;
+
+      if (root == NULL || !search(&root->right->key)) {
+        return;
+      }
+
+      path.push_back(node);
+      node = &root->right;
+    }
+  }
   // Recursive function to insert a key in the subtree rooted
   // with node and returns the new root of the subtree.
-  static void insert_node(Node<T> **node, T key, KeyOrdering<T> compare) {
+  static void find(Node<T> **node, KeySearch<T> search,
+                   std::vector<Node<T> **> &path) {
+    while (true) {
+      path.push_back(node);
+
+      Node<T> *root = *node;
+
+      if (root == NULL) {
+        return;
+      }
+
+      int ordering = search(&root->key);
+      if (ordering < 0) {
+        node = &root->left;
+      } else if (ordering > 0) {
+        node = &root->right;
+      } else {
+        return;
+      }
+    }
+  }
+  static void rebalance_path(std::vector<Node<T> **> &path) {
+    for (auto it = path.rbegin(); it != path.rend(); ++it) {
+      maybe_rebalance_node(it);
+    }
+  }
+  // Recursive function to insert a key in the subtree rooted
+  // with node and returns the new root of the subtree.
+  static void insert(Node<T> **node, T key, KeyOrdering<T> compare) {
     Node<T> *root = *node;
 
     // if inserting into empty, create new node
@@ -115,9 +175,9 @@ template <typename T> struct Node {
 
     int ordering = compare(&key, &root->key);
     if (ordering < 0) {
-      insert_node(&root->left, key, compare);
+      insert(&root->left, key, compare);
     } else if (ordering > 0) {
-      insert_node(&root->right, key, compare);
+      insert(&root->right, key, compare);
     } else {
       root->key = key;
       return;
@@ -135,7 +195,7 @@ template <typename T> struct Node {
     }
     return prev;
   }
-  static void delete_node(Node **node, int key, KeyOrdering<T> compare) {
+  static void remove(Node **node, T &key, KeyOrdering<T> compare) {
     Node *root = *node;
 
     if (root == NULL) {
@@ -144,9 +204,9 @@ template <typename T> struct Node {
 
     int ordering = compare(&key, &root->key);
     if (ordering < 0) {
-      delete_node(&root->left, key, compare);
+      remove(&root->left, key, compare);
     } else if (ordering > 0) {
-      delete_node(&root->right, key, compare);
+      remove(&root->right, key, compare);
     } else {
       if (root->left && root->right) {
         // get the inorder successor (smallest in the right subtree)
@@ -154,7 +214,7 @@ template <typename T> struct Node {
         // leftmost is now root
         root->key = leftmost->key;
         // delete the old leftmost, this is a bit inelegant
-        delete_node(&root->right, leftmost->key, compare);
+        remove(&root->right, leftmost->key, compare);
       } else {
         if (root->left) {
           *node = root->left;
@@ -205,8 +265,15 @@ public:
     }
   }
 
-  void insert(T key) { Node<T>::insert_node(&this->root, key, this->compare); }
-  void remove(T key) { Node<T>::delete_node(&this->root, key, this->compare); }
+  void find(KeySearch<T> search, std::vector<Node<T> **> &path) {
+    path.clear();
+    Node<T>::find(&this->root, search, path);
+  }
+  void rebalance_path(std::vector<Node<T> **> &path) {
+    Node<T>::rebalance_path(&this->root, path);
+  }
+  void insert(T key) { Node<T>::insert(&this->root, key, this->compare); }
+  void remove(T key) { Node<T>::remove(&this->root, key, this->compare); }
   void print() const { _node_print(root, 0); }
   void print_order() const { Node<T>::_node_print_order(root, 0); }
 };
@@ -218,6 +285,7 @@ private:
   std::vector<Node<T> *> stack;
 
 public:
+  AvlIterator() {}
   AvlIterator(Node<T> *root) { moveLeft(root); }
 
   void moveLeft(Node<T> *current) {
@@ -245,6 +313,7 @@ public:
 
 int make_value(int i) { return (i * i * 127 + ~i) % 64; }
 
+#if false
 int main() {
   auto compare = [](int *a, int *b) -> int { return *a - *b; };
   AvlTree<int> root(compare);
@@ -265,3 +334,4 @@ int main() {
   root.print_order();
   return 0;
 }
+#endif
