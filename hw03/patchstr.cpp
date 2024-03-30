c418116ce74085d8d277c397e481e6a600470050
Michal Havlíček <havlim39@fit.cvut.cz>
2024-03-30 09:38:55 +0100

    Initial interface

--- hw03/patchstr.cpp
+++ hw03/patchstr.cpp
@@ -10,27 +10,158 @@
     #include <stdexcept>
 #endif /* __PROGTEST__ */
 
+class CPatchStr;
+
+class RcString {
+    unsigned* count;
+
+  public:
+    RcString(const char* str, size_t len) {
+        size_t bytes = len + sizeof(unsigned)
+            + 1;  // including refcount and terminating \0
+        char* data = (char*)malloc(bytes);
+
+        *(unsigned*)data = 1;
+        memcpy(data + 4, str, len);
+    }
+
+    RcString(const RcString& a) : count(a.count) {
+        (*a.count)++;
+    }
+
+    RcString(RcString&& a) : count(a.count) {
+        a.count = nullptr;
+    }
+
+    ~RcString() {
+        if (count) {
+            if (--(*count) == 0) {
+                free(count);
+            }
+        }
+    }
+
+    const RcString& operator=(RcString other) {
+        *this = other;
+        return *this;
+    }
+
+    unsigned refcount() const {
+        return *count;
+    }
+
+    const char* get() const {
+        return (const char*)(count + 4);
+    }
+};
+
+class Part {
+    unsigned offset;
+    unsigned size;
+    RcString data;
+
+  public:
+    Part(const char* str) :
+        offset(0),
+        size(strlen(str)),
+        data(RcString(str, size)) {}
+
+    void remove_start(unsigned count) {
+        assert(count <= size);
+        offset += count;
+        size -= count;
+    }
+
+    void remove_end(unsigned count) {
+        assert(count <= size);
+        size -= count;
+    }
+
+    bool empty() const {
+        return size == 0;
+    }
+};
+
+struct ListElement {
+    Part self;
+    ListElement* next = nullptr;
+};
+
+class List {
+    ListElement* head;
+
+  public:
+    List() {}
+
+    List(List&& a) : head(a.head) {
+        a.head = nullptr;
+    }
+
+    List(List& a) {
+        ListElement** next = &head;
+        ListElement* cursor = a.head;
+        while (cursor) {
+            ListElement* copy = new ListElement(*cursor);
+            *next = copy;
+
+            next = &copy->next;
+            cursor = cursor->next;
+        }
+    }
+
+    List(Part value) : head(new ListElement {value, nullptr}) {}
+
+    ~List() {
+        ListElement* cursor = head;
+        while (cursor) {
+            ListElement* tmp = cursor;
+            cursor = tmp->next;
+            free(tmp);
+        }
+    }
+};
+
 class CPatchStr {
+    List parts;
+
   public:
     CPatchStr();
-    CPatchStr(const char* str);
+
+    CPatchStr(CPatchStr& a) : parts(a.parts) {}
+
+    CPatchStr(CPatchStr&& a) : parts(std::move(a.parts)) {}
+
+    CPatchStr(const char* str) : parts(List(Part(str))) {}
+
+    void operator=(CPatchStr other) {
+        *this = other;
+    }
+
+    void operator=(const char* str) {
+        *this = CPatchStr(str);
+    }
+
     // copy constructor
     // destructor
     // operator =
-    CPatchStr subStr(size_t from, size_t len) const;
+    CPatchStr subStr(size_t from, size_t len) const {
+        return "";
+    }
+
     CPatchStr& append(const CPatchStr& src);
 
     CPatchStr& insert(size_t pos, const CPatchStr& src);
     CPatchStr& remove(size_t from, size_t len);
     char* toStr() const;
-
-  private:
-    // todo
 };
 
 #ifndef __PROGTEST__
 bool stringMatch(char* str, const char* expected) {
     bool res = std::strcmp(str, expected) == 0;
+    if (!res) {
+        std::cout << "expected: " << str << "\ngot:      " << expected
+                  << std::endl;
+    }
     delete[] str;
     return res;
 }
