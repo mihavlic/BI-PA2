f7f49f4a85d9b5e2652a37848afd2163f41d3f65
Michal Havlíček <havlim39@fit.cvut.cz>
2024-04-21 22:00:34 +0200

    hw05 now works

--- hw05/evidence.cpp
+++ hw05/evidence.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #ifndef __PROGTEST__
     #include <algorithm>
     #include <cassert>
@@ -13,15 +14,288 @@
     #include <vector>
 #endif /* __PROGTEST__ */
 
-class CComputer {};
+struct Scope {
+    bool is_last;
+};
 
-class CNetwork {};
+class Thing {
+    std::vector<Thing*> children;
 
-class CCPU {};
+  public:
+    virtual Thing* clone() const {
+        return new Thing(*this);
+    }
 
-class CMemory {};
+    Thing() {}
 
-class CDisk {};
+    Thing(const Thing& other) : children(other.children.size(), nullptr) {
+        for (size_t i = 0; i < other.children.size(); i++) {
+            children[i] = other.children[i]->clone();
+        }
+    }
+
+    virtual ~Thing() {
+        for (auto child : children) {
+            delete child;
+        }
+    }
+
+    Thing& operator=(const Thing& other) {
+        if (this == &other) {
+            return *this;
+        }
+        Thing copy(other);
+        for (auto child : children) {
+            delete child;
+        }
+        children = std::move(copy.children);
+        return *this;
+    }
+
+    virtual void add_child(Thing* owned_child_ptr) {
+        children.push_back(owned_child_ptr);
+    }
+
+    virtual void
+    print_self(std::ostream& os, std::vector<Scope>& scopes) const {}
+
+    void print_children(std::ostream& os, std::vector<Scope>& scopes) const {
+        scopes.push_back({false});
+        for (size_t i = 0; i < children.size(); i++) {
+            scopes.back().is_last = (i + 1) == children.size();
+            children[i]->print_recursive(os, scopes);
+        }
+        scopes.pop_back();
+    }
+
+    virtual void
+    print_recursive(std::ostream& os, std::vector<Scope>& scopes) const {
+        for (int i = 0; i < ((int)scopes.size()) - 1; i++) {
+            if (scopes[i].is_last) {
+                os << "  ";
+            } else {
+                os << "| ";
+            }
+        }
+
+        if (!scopes.empty()) {
+            if (scopes.back().is_last) {
+                os << "\\-";
+            } else {
+                os << "+-";
+            }
+        }
+
+        print_self(os, scopes);
+        os << std::endl;
+        print_children(os, scopes);
+    }
+
+    const std::vector<Thing*>& get_children() const {
+        return children;
+    }
+
+    std::vector<Thing*>& get_children() {
+        return children;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Thing& self) {
+        std::vector<Scope> scopes {};
+        self.print_recursive(os, scopes);
+        return os;
+    }
+};
+
+class Address: public Thing {
+    std::string address;
+
+  public:
+    Thing* clone() const override {
+        return new Address(*this);
+    }
+
+    Address(std::string address) : address(address) {}
+
+    void
+    print_self(std::ostream& os, std::vector<Scope>& scopes) const override {
+        os << address;
+    }
+};
+
+class CCPU: public Thing {
+    unsigned core_count;
+    unsigned mhz;
+
+  public:
+    Thing* clone() const override {
+        return new CCPU(*this);
+    }
+
+    CCPU(unsigned core_count, unsigned mhz) :
+        core_count(core_count),
+        mhz(mhz) {}
+
+    void
+    print_self(std::ostream& os, std::vector<Scope>& scopes) const override {
+        // CPU, 8 cores @ 1200MHz
+        os << "CPU, " << core_count << " cores @ " << mhz << "MHz";
+    }
+};
+
+class CMemory: public Thing {
+    unsigned mib;
+
+  public:
+    Thing* clone() const override {
+        return new CMemory(*this);
+    }
+
+    CMemory(unsigned mib) : mib(mib) {}
+
+    void
+    print_self(std::ostream& os, std::vector<Scope>& scopes) const override {
+        // Memory, 2000 MiB
+        os << "Memory, " << mib << " MiB";
+    }
+};
+
+class Partition: public Thing {
+    unsigned index;
+    unsigned gib;
+    std::string path;
+
+  public:
+    Thing* clone() const override {
+        return new Partition(*this);
+    }
+
+    Partition(unsigned index, unsigned gib, std::string path) :
+        index(index),
+        gib(gib),
+        path(path) {}
+
+    void
+    print_self(std::ostream& os, std::vector<Scope>& scopes) const override {
+        // [0]: 60 GiB, /data
+        os << "[" << index << "]: " << gib << " GiB, " << path;
+    }
+};
+
+class CDisk: public Thing {
+  public:
+    Thing* clone() const override {
+        return new CDisk(*this);
+    }
+
+    enum DiskType { SSD, MAGNETIC };
+
+    CDisk(DiskType disk_type, unsigned gib) : disk_type(disk_type), gib(gib) {}
+
+    CDisk& addPartition(unsigned gib, std::string path) {
+        add_child(new Partition(this->get_children().size(), gib, path));
+        return *this;
+    }
+
+    void
+    print_self(std::ostream& os, std::vector<Scope>& scopes) const override {
+        // HDD, 1500 GiB
+        if (disk_type == SSD) {
+            os << "SSD, ";
+        } else {
+            os << "HDD, ";
+        }
+        os << gib << " GiB";
+    }
+
+  private:
+    DiskType disk_type;
+    unsigned gib;
+};
+
+class CComputer: public Thing {
+    std::string name;
+
+  public:
+    Thing* clone() const override {
+        return new CComputer(*this);
+    }
+
+    CComputer(std::string name) : name(name) {}
+
+    CComputer& addComponent(CCPU child) {
+        add_child(new CCPU(child));
+        return *this;
+    }
+
+    CComputer& addComponent(CMemory child) {
+        add_child(new CMemory(child));
+        return *this;
+    }
+
+    CComputer& addComponent(CDisk child) {
+        add_child(new CDisk(child));
+        return *this;
+    }
+
+    CComputer& addAddress(std::string child) {
+        add_child(new Address(child));
+        return *this;
+    }
+
+    void
+    print_self(std::ostream& os, std::vector<Scope>& scopes) const override {
+        std::vector<Thing*>& children =
+            *const_cast<std::vector<Thing*>*>(&get_children());
+
+        // 10x coder right here
+        std::stable_sort(
+            children.begin(),
+            children.end(),
+            [](Thing* a, Thing* b) -> bool {
+                return dynamic_cast<Address*>(a) != 0
+                    && dynamic_cast<Address*>(b) == 0;
+            }
+        );
+
+        // Host: progtest.fit.cvut.cz
+        os << "Host: " << name;
+    }
+
+    friend class CNetwork;
+};
+
+class CNetwork: public Thing {
+    std::string name;
+
+  public:
+    Thing* clone() const override {
+        return new CNetwork(*this);
+    }
+
+    CNetwork(std::string name) : name(name) {}
+
+    CNetwork& addComputer(CComputer child) {
+        add_child(new CComputer(child));
+        return *this;
+    }
+
+    CComputer* findComputer(const std::string& name) {
+        for (auto child : this->get_children()) {
+            CComputer* computer = dynamic_cast<CComputer*>(child);
+
+            if (computer->name == name) {
+                return computer;
+            }
+        }
+        return nullptr;
+    }
+
+    void
+    print_self(std::ostream& os, std::vector<Scope>& scopes) const override {
+        // Network: FIT network
+        os << "Network: " << name;
+    }
+};
 
 #ifndef __PROGTEST__
 template<typename T_>
@@ -71,30 +345,30 @@ int main() {
                          .addComponent(CMemory(8000)));
     compare(
         n,
-    "Network: FIT network\n"
-    "+-Host: progtest.fit.cvut.cz\n"
-    "| +-147.32.232.142\n"
-    "| +-CPU, 8 cores @ 2400MHz\n"
-    "| +-CPU, 8 cores @ 1200MHz\n"
-    "| +-HDD, 1500 GiB\n"
-    "| | +-[0]: 50 GiB, /\n"
-    "| | +-[1]: 5 GiB, /boot\n"
-    "| | \\-[2]: 1000 GiB, /var\n"
-    "| +-SSD, 60 GiB\n"
-    "| | \\-[0]: 60 GiB, /data\n"
-    "| +-Memory, 2000 MiB\n"
-    "| \\-Memory, 2000 MiB\n"
-    "+-Host: courses.fit.cvut.cz\n"
-    "| +-147.32.232.213\n"
-    "| +-CPU, 4 cores @ 1600MHz\n"
-    "| +-Memory, 4000 MiB\n"
-    "| \\-HDD, 2000 GiB\n"
-    "|   +-[0]: 100 GiB, /\n"
-    "|   \\-[1]: 1900 GiB, /data\n"
-    "\\-Host: imap.fit.cvut.cz\n"
-    "  +-147.32.232.238\n"
-    "  +-2001:718:2:2901::238\n"
-    "  +-CPU, 4 cores @ 2500MHz\n"
+        "Network: FIT network\n"
+        "+-Host: progtest.fit.cvut.cz\n"
+        "| +-147.32.232.142\n"
+        "| +-CPU, 8 cores @ 2400MHz\n"
+        "| +-CPU, 8 cores @ 1200MHz\n"
+        "| +-HDD, 1500 GiB\n"
+        "| | +-[0]: 50 GiB, /\n"
+        "| | +-[1]: 5 GiB, /boot\n"
+        "| | \\-[2]: 1000 GiB, /var\n"
+        "| +-SSD, 60 GiB\n"
+        "| | \\-[0]: 60 GiB, /data\n"
+        "| +-Memory, 2000 MiB\n"
+        "| \\-Memory, 2000 MiB\n"
+        "+-Host: courses.fit.cvut.cz\n"
+        "| +-147.32.232.213\n"
+        "| +-CPU, 4 cores @ 1600MHz\n"
+        "| +-Memory, 4000 MiB\n"
+        "| \\-HDD, 2000 GiB\n"
+        "|   +-[0]: 100 GiB, /\n"
+        "|   \\-[1]: 1900 GiB, /data\n"
+        "\\-Host: imap.fit.cvut.cz\n"
+        "  +-147.32.232.238\n"
+        "  +-2001:718:2:2901::238\n"
+        "  +-CPU, 4 cores @ 2500MHz\n"
         "  \\-Memory, 8000 MiB\n"
     );
     n = n;
@@ -102,10 +376,10 @@ int main() {
     auto c = x.findComputer("imap.fit.cvut.cz");
     compare(
         *c,
-    "Host: imap.fit.cvut.cz\n"
-    "+-147.32.232.238\n"
-    "+-2001:718:2:2901::238\n"
-    "+-CPU, 4 cores @ 2500MHz\n"
+        "Host: imap.fit.cvut.cz\n"
+        "+-147.32.232.238\n"
+        "+-2001:718:2:2901::238\n"
+        "+-CPU, 4 cores @ 2500MHz\n"
         "\\-Memory, 8000 MiB\n"
     );
     c->addComponent(CDisk(CDisk::MAGNETIC, 1000)
@@ -114,62 +388,62 @@ int main() {
                         .addPartition(700, "mail"));
     compare(
         x,
-    "Network: FIT network\n"
-    "+-Host: progtest.fit.cvut.cz\n"
-    "| +-147.32.232.142\n"
-    "| +-CPU, 8 cores @ 2400MHz\n"
-    "| +-CPU, 8 cores @ 1200MHz\n"
-    "| +-HDD, 1500 GiB\n"
-    "| | +-[0]: 50 GiB, /\n"
-    "| | +-[1]: 5 GiB, /boot\n"
-    "| | \\-[2]: 1000 GiB, /var\n"
-    "| +-SSD, 60 GiB\n"
-    "| | \\-[0]: 60 GiB, /data\n"
-    "| +-Memory, 2000 MiB\n"
-    "| \\-Memory, 2000 MiB\n"
-    "+-Host: courses.fit.cvut.cz\n"
-    "| +-147.32.232.213\n"
-    "| +-CPU, 4 cores @ 1600MHz\n"
-    "| +-Memory, 4000 MiB\n"
-    "| \\-HDD, 2000 GiB\n"
-    "|   +-[0]: 100 GiB, /\n"
-    "|   \\-[1]: 1900 GiB, /data\n"
-    "\\-Host: imap.fit.cvut.cz\n"
-    "  +-147.32.232.238\n"
-    "  +-2001:718:2:2901::238\n"
-    "  +-CPU, 4 cores @ 2500MHz\n"
-    "  +-Memory, 8000 MiB\n"
-    "  \\-HDD, 1000 GiB\n"
-    "    +-[0]: 100 GiB, system\n"
-    "    +-[1]: 200 GiB, WWW\n"
+        "Network: FIT network\n"
+        "+-Host: progtest.fit.cvut.cz\n"
+        "| +-147.32.232.142\n"
+        "| +-CPU, 8 cores @ 2400MHz\n"
+        "| +-CPU, 8 cores @ 1200MHz\n"
+        "| +-HDD, 1500 GiB\n"
+        "| | +-[0]: 50 GiB, /\n"
+        "| | +-[1]: 5 GiB, /boot\n"
+        "| | \\-[2]: 1000 GiB, /var\n"
+        "| +-SSD, 60 GiB\n"
+        "| | \\-[0]: 60 GiB, /data\n"
+        "| +-Memory, 2000 MiB\n"
+        "| \\-Memory, 2000 MiB\n"
+        "+-Host: courses.fit.cvut.cz\n"
+        "| +-147.32.232.213\n"
+        "| +-CPU, 4 cores @ 1600MHz\n"
+        "| +-Memory, 4000 MiB\n"
+        "| \\-HDD, 2000 GiB\n"
+        "|   +-[0]: 100 GiB, /\n"
+        "|   \\-[1]: 1900 GiB, /data\n"
+        "\\-Host: imap.fit.cvut.cz\n"
+        "  +-147.32.232.238\n"
+        "  +-2001:718:2:2901::238\n"
+        "  +-CPU, 4 cores @ 2500MHz\n"
+        "  +-Memory, 8000 MiB\n"
+        "  \\-HDD, 1000 GiB\n"
+        "    +-[0]: 100 GiB, system\n"
+        "    +-[1]: 200 GiB, WWW\n"
         "    \\-[2]: 700 GiB, mail\n"
     );
     compare(
         n,
-    "Network: FIT network\n"
-    "+-Host: progtest.fit.cvut.cz\n"
-    "| +-147.32.232.142\n"
-    "| +-CPU, 8 cores @ 2400MHz\n"
-    "| +-CPU, 8 cores @ 1200MHz\n"
-    "| +-HDD, 1500 GiB\n"
-    "| | +-[0]: 50 GiB, /\n"
-    "| | +-[1]: 5 GiB, /boot\n"
-    "| | \\-[2]: 1000 GiB, /var\n"
-    "| +-SSD, 60 GiB\n"
-    "| | \\-[0]: 60 GiB, /data\n"
-    "| +-Memory, 2000 MiB\n"
-    "| \\-Memory, 2000 MiB\n"
-    "+-Host: courses.fit.cvut.cz\n"
-    "| +-147.32.232.213\n"
-    "| +-CPU, 4 cores @ 1600MHz\n"
-    "| +-Memory, 4000 MiB\n"
-    "| \\-HDD, 2000 GiB\n"
-    "|   +-[0]: 100 GiB, /\n"
-    "|   \\-[1]: 1900 GiB, /data\n"
-    "\\-Host: imap.fit.cvut.cz\n"
-    "  +-147.32.232.238\n"
-    "  +-2001:718:2:2901::238\n"
-    "  +-CPU, 4 cores @ 2500MHz\n"
+        "Network: FIT network\n"
+        "+-Host: progtest.fit.cvut.cz\n"
+        "| +-147.32.232.142\n"
+        "| +-CPU, 8 cores @ 2400MHz\n"
+        "| +-CPU, 8 cores @ 1200MHz\n"
+        "| +-HDD, 1500 GiB\n"
+        "| | +-[0]: 50 GiB, /\n"
+        "| | +-[1]: 5 GiB, /boot\n"
+        "| | \\-[2]: 1000 GiB, /var\n"
+        "| +-SSD, 60 GiB\n"
+        "| | \\-[0]: 60 GiB, /data\n"
+        "| +-Memory, 2000 MiB\n"
+        "| \\-Memory, 2000 MiB\n"
+        "+-Host: courses.fit.cvut.cz\n"
+        "| +-147.32.232.213\n"
+        "| +-CPU, 4 cores @ 1600MHz\n"
+        "| +-Memory, 4000 MiB\n"
+        "| \\-HDD, 2000 GiB\n"
+        "|   +-[0]: 100 GiB, /\n"
+        "|   \\-[1]: 1900 GiB, /data\n"
+        "\\-Host: imap.fit.cvut.cz\n"
+        "  +-147.32.232.238\n"
+        "  +-2001:718:2:2901::238\n"
+        "  +-CPU, 4 cores @ 2500MHz\n"
         "  \\-Memory, 8000 MiB\n"
     );
     return EXIT_SUCCESS;
